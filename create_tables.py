d6b0b2a42d1154b5b6ab7c0fac02ff54f47eb0e9
himanshu5223 <41504460+himanshu5223@users.noreply.github.com>
2020-09-26 16:30:20 +0530

    Add files via upload

---
+++ create_tables.py
@@ -0,0 +1,17 @@
+import sqlite3
+
+connection = sqlite3.connect('data.db')
+cursor = connection.cursor()
+
+create_table = "create table if not exists users(id INTEGER PRIMARY KEY, username text,password text)"
+cursor.execute(create_table)
+
+
+create_table = "create table if not exists items (name text,price real)"
+cursor.execute(create_table)
+ 
+
+
+connection.commit()
+connection.close()
+
